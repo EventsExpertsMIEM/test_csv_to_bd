85905828f3036440b9a0d2b6e5f319c8f7f046e0
aaschupak <aaschupak@miem.hse.ru>
2020-03-09 02:00:33 +0300

    Updated tables for SOHE and code for pushing csv

--- models_test.py
+++ models_test.py
@@ -44,60 +44,38 @@ class Specialities_of_higher_education(Base):
     full_name = Column(String, nullable=False)
 
 
-class SOHE_bakalavriat(Base):
-    __tablename__ = 'SOHE_b'
+Level = ENUM('b', 'm', 's', 'ap', 'as',
+             name='level')
+
+
+class SOHE_additional(Base):
+    __tablename__ = 'SOHE_additional'
 
     area_id = Column(Integer, ForeignKey(
         'Spec_of_h_ed.id'), nullable=False)
-    id = Column(String, primary_key=True)
-    full_name = Column(String, nullable=False)
-
-
-class SOHE_magistratura(Base):
-    __tablename__ = 'SOHE_m'
-
-    area_id = Column(Integer, ForeignKey(
-        'Spec_of_h_ed.id'), nullable=False)
-    id = Column(String, primary_key=True)
-    full_name = Column(String, nullable=False)
-
-
-class SOHE_aspirantura(Base):
-    __tablename__ = 'SOHE_aspir'
-
-    area_id = Column(Integer, ForeignKey(
-        'Spec_of_h_ed.id'), nullable=False)
-    id = Column(String, primary_key=True)
-    full_name = Column(String, nullable=False)
-
-
-class SOHE_assistentura(Base):
-    __tablename__ = 'SOHE_assis'
-
-    area_id = Column(Integer, ForeignKey(
-        'Spec_of_h_ed.id'), nullable=False)
-    id = Column(String, primary_key=True)
+    level = Column(Level, nullable=False)
+    id = Column(Integer, primary_key=True)
     full_name = Column(String, nullable=False)
 
 
 engine = create_engine('sqlite:///testbase.db')
 Base.metadata.create_all(engine)
 
-
+# main tables
 list_of_csv_files = ('Scientific_areas.csv',
                      'Specialities_of_higher_education.csv')
-list_of_csv_add_files = ('Scientific_areas_add.csv',
-                         'SOHE_bakalavriat.csv', 'SOHE_magistratura.csv', 'SOHE_aspirantura.csv', 'SOHE_assistentura.csv')
 list_of_tables = ('Scientific_areas', 'Spec_of_h_ed', )
-list_of_add_tables = ('Scientific_areas_additional',
-                      'SOHE_b', 'SOHE_m', 'SOHE_aspir', 'SOHE_assis')
 
 for csv, table in zip(list_of_csv_files, list_of_tables):
     df = pd.read_csv(csv, sep=';')
     df.to_sql(table, con=engine, index_label='id',
               index=False, if_exists='replace')
 
-for csv, table in zip(list_of_csv_add_files, list_of_add_tables):
-    df = pd.read_csv(csv, sep=';')
-    df.to_sql(table, index_label='id', index=False,
-              con=engine, if_exists='replace')
+# additional tables
+df = pd.read_csv('Scientific_areas_add.csv', sep=';')
+df.to_sql('Scientific_areas_additional', con=engine, index_label='id',
+          index=False, if_exists='replace')
+
+df = pd.read_csv('SOHE_additional.csv', sep=';')
+df.to_sql('SOHE_additional', con=engine, index_label='id',
+          index=False, if_exists='replace')
